d1417940d5382ee9b1fefce6598764688e7f8241
Xiang <qasdfgtyuiop@gmail.com>
2021-03-03 12:37:54 -0800

    save

--- test.cpp
+++ test.cpp
@@ -3,6 +3,12 @@
 using torch::Tensor;
 using torch::autograd::tensor_list;
 
+struct Container : torch::CustomClassHolder {
+  Tensor x;
+  Container() {}
+  Container(Tensor x):x(x) {}
+};
+
 Tensor forward_cpu(Tensor x) {
     return torch::ones_like(x);
 }
@@ -11,11 +17,12 @@ class MyCustomFunc : public torch::autograd::Function<MyCustomFunc> {
  public:
   static Tensor forward(torch::autograd::AutogradContext* ctx, Tensor x) {
       at::AutoNonVariableTypeMode g;
+      ctx->saved_data["x"] = c10::make_intrusive<Container>(torch::randn_like(x));
       return forward_cpu(x);
   }
   static tensor_list backward(torch::autograd::AutogradContext* ctx, tensor_list grad_outputs) {
       at::AutoNonVariableTypeMode g;
-      return {forward_cpu(grad_outputs[0])};
+      return {grad_outputs[0] * MyCustomFunc::apply(ctx->saved_data["x"].toCustomClass<Container>()->x)};
   }
 };
 
@@ -24,10 +31,12 @@ Tensor forward_autograd(Tensor x) {
 }
 
 TORCH_LIBRARY(test, m) {
+  m.class_<Container>("Container").def(
+      torch::init<Tensor>());
   m.def("forward", forward_cpu);
 }
 
-TORCH_LIBRARY_IMPL(test, CPU, m) {
+TORCH_LIBRARY_IMPL(test, DefaultBackend, m) {
   m.impl("forward", forward_cpu);
 }
 
--- test.py
+++ test.py
@@ -2,3 +2,9 @@ import torch
 import torch.utils.cpp_extension
 
 torch.ops.load_library('build/lib.macosx-11-arm64-3.9/test.cpython-39-darwin.so')
+
+for i in range(100000):
+    x = torch.zeros(1024*1024*512, requires_grad=True)
+    y = torch.ops.test.forward(x)
+    g = torch.autograd.grad(y, x, y, create_graph=True, retain_graph=True)
+    g.sum().backward()
