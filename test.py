79e26e81cd5fe847b64b4bb09fc14ebd823b0698
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-03-03 16:01:59 -0800

    save

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import torch
 import tqdm
-import torch.utils.cpp_extension
+import glob
 
-torch.ops.load_library('build/lib.macosx-11-arm64-3.9/test.cpython-39-darwin.so')
+g = glob.glob("build/*/*.so")
+for f in g:
+    if f.endswith(".so"):
+        break
+
+torch.ops.load_library(f)
 
 for i in tqdm.trange(100000):
     x = torch.zeros(1024*1024*512, requires_grad=True)
