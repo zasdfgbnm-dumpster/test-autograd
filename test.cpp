ff3d175726a917842ac566b36702ef14e2e29c5c
richard <yueyericardo@gmail.com>
2021-03-03 19:12:37 -0500

    leak

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include <torch/extension.h>
+#include <iostream>
 
 using torch::Tensor;
 using torch::autograd::tensor_list;
@@ -7,15 +8,21 @@ struct Container : torch::CustomClassHolder {
   Tensor x;
   Container() {}
   Container(Tensor x):x(x) {}
+  void release_resources() {
+    std::cout << "--------ptr release-------- " <<"\n";
+  }
 };
 
 class MyCustomFunc : public torch::autograd::Function<MyCustomFunc> {
  public:
   static Tensor forward(torch::autograd::AutogradContext* ctx, Tensor x) {
       at::AutoNonVariableTypeMode g;
-      auto ptr = c10::make_intrusive<Container>(torch::randn_like(x));
+      Container container = {torch::randn_like(x)};
+      auto ptr = c10::make_intrusive<Container>(container);
       ctx->saved_data["x"] = ptr;
-      return ptr->x;
+      Tensor t = container.x.clone().detach();
+      return container.x;  // blow up, release_resources() got called, but ptr memory doesn't got free
+      // return t;         // ptr memory got free
   }
   static tensor_list backward(torch::autograd::AutogradContext* ctx, tensor_list grad_outputs) {
       return {grad_outputs[0] * MyCustomFunc::apply(ctx->saved_data["x"].toCustomClass<Container>()->x)};
@@ -26,13 +33,13 @@ Tensor forward_autograd(Tensor x) {
    return MyCustomFunc::apply(x);
 }
 
-TORCH_LIBRARY(test, m) {
+TORCH_LIBRARY(test1, m) {
   m.class_<Container>("Container").def(
       torch::init<Tensor>());
   m.def("forward", forward_autograd);
 }
 
-TORCH_LIBRARY_IMPL(test, Autograd, m) {
+TORCH_LIBRARY_IMPL(test1, Autograd, m) {
   m.impl("forward", forward_autograd);
 }
 
--- test.py
+++ test.py
@@ -1,11 +1,31 @@
 import torch
-import tqdm
+import pynvml
+import os
 import torch.utils.cpp_extension
 
 torch.ops.load_library('build/lib.macosx-11-arm64-3.9/test.cpython-39-darwin.so')
 
-for i in tqdm.trange(100000):
-    x = torch.zeros(1024*1024*512, requires_grad=True)
-    y = torch.ops.test.forward(x).sum()
-    g = torch.autograd.grad(y, x, create_graph=True, retain_graph=True)[0]
+
+def checkgpu(device=None):
+    i = device if device else torch.cuda.current_device()
+    t = torch.cuda.get_device_properties(i).total_memory
+    c = torch.cuda.memory_reserved(i)
+    name = torch.cuda.get_device_properties(i).name
+    print('   GPU Memory Cached (pytorch) : {:7.1f}MB / {:.1f}MB ({})'.format(c / 1024 / 1024, t / 1024 / 1024, name))
+    real_i = int(os.environ['CUDA_VISIBLE_DEVICES'][0]) if 'CUDA_VISIBLE_DEVICES' in os.environ else i
+    pynvml.nvmlInit()
+    h = pynvml.nvmlDeviceGetHandleByIndex(real_i)
+    info = pynvml.nvmlDeviceGetMemoryInfo(h)
+    name = pynvml.nvmlDeviceGetName(h)
+    print('   GPU Memory Used (nvidia-smi): {:7.1f}MB / {:.1f}MB ({})'.format(info.used / 1024 / 1024, info.total / 1024 / 1024, name.decode()))
+    return f'{(info.used / 1024 / 1024):.1f}MB'
+
+
+for i in range(10):
+    x = torch.zeros(1024*1024*50, requires_grad=True, device='cuda')
+    y = torch.ops.test1.forward(x)
+    g = torch.autograd.grad(y, x, y, create_graph=True, retain_graph=True)[0]
     g.sum().backward()
+    print(i)
+    checkgpu()
+    print('-' * 70)
